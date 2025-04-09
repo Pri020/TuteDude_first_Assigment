70bd76e68002649c1de9c3a53e3415d526816311
Pri020 <89369747+Pri020@users.noreply.github.com>
2025-04-09 12:51:53 +0530

    Add files via upload

---
+++ first_1.py
@@ -0,0 +1,10 @@
+num1 = input("Enter First number: ")
+num2 = input("Enter Second number: ")
+num1 = float(num1)
+num2 = float(num2)
+
+
+print("Addition: ", num1 + num2)
+print("Subtraction: ", num1 - num2)
+print("Multiplication: ", num1 * num2)
+print("Division: ", num1 / num2)
